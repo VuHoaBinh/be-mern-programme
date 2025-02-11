338f22d7dd592302c2d4337b92e5650cf8b2f783
VuHoaBinh <hoabinh.vippro63@gmail.com>
2025-02-11 16:52:58 +0700

    feat: update place

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const placesRoutes = require("./routes/places-routes");
-const HttpError = require("./models/http-error");
+const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/api/places", placesRoutes); // => /api/places...
+app.use('/api/places', placesRoutes); // => /api/places...
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
+  const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
@@ -18,8 +19,8 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
 });
 
 app.listen(5000);
